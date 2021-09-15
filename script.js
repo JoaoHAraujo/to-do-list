bde41126788a683833fee9158309fba1562d5f03
JoaoHAraujo <j.henr@outlook.com>
2021-09-14 21:17:47 -0300

    organizando post e html

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const btnSave = document.querySelector('#saveTask')
 const urlToDo = "http://localhost:3000/to_do"
 
 btnSave.addEventListener('click', function() {
-    const input = document.querySelector('#newTask')
-    const task = input.value
+    const newTask = document.querySelector('#newTask').value
+    const newDescription = document.querySelector('#description').value
+    const prioritySelect = document.querySelector('#priority')
+    const newPriority = prioritySelect.options[prioritySelect.selectedIndex].text
+
     const submitTask = {
         method: 'POST',
-        body: `{"nome": "${task}", "descricao": "a definir", "urgencia": 0}`,
+        body: `{"nome": "${newTask}", "descricao": "${newDescription}", "urgencia": "${newPriority}"}`,
         headers: {
             "Content-Type": "application/json; charset=utf-8"
         }
