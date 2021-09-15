43183012fd6bd07d4d4890be05fcb1349cc8f86f
JoaoHAraujo <j.henr@outlook.com>
2021-09-14 23:03:14 -0300

    Mostrando Lista

--- script.js
+++ script.js
@@ -11,10 +11,44 @@ btnSave.addEventListener('click', function() {
 
     const submitTask = {
         method: 'POST',
-        body: `{"nome": "${newTask}", "descricao": "${newDescription}", "urgencia": "${newPriority}"}`,
         headers: {
             "Content-Type": "application/json; charset=utf-8"
-        }
+        },
+        body: JSON.stringify(
+            {
+                task: newTask,
+                description: newDescription,
+                priority: newPriority
+            }
+        )
     }
     fetch(urlToDo, submitTask)
 })
+
+
+const taskList = document.querySelector('#taskList')
+
+fetch(urlToDo)
+    .then(res => res.json())
+    .then(toDoList => {
+
+        toDoList.map(tasks => {
+            const item = document.createElement('li')
+
+            const itemTask = document.createElement('h2')
+            itemTask.textContent = tasks.task
+
+            const itemDescription = document.createElement('p')
+            itemDescription.textContent = `Descrição: ${tasks.description}`
+
+            const itemPriority = document.createElement('p')
+            itemPriority.textContent = `Prioridade: ${tasks.priority}`
+
+            item.appendChild(itemTask)
+            item.appendChild(itemDescription)
+            item.appendChild(itemPriority)
+            taskList.appendChild(item)
+            
+        })   
+    })
+
