0dde9eed045d157ddc23d0667f9821e163a42ee9
JoaoHAraujo <j.henr@outlook.com>
2021-09-17 22:55:02 -0300

    função remove implementada

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const btnSave = document.querySelector('#saveTask')
-
-
-const urlToDo = "http://localhost:3000/to_do"
+const urlToDo = "http://localhost:3000/to_do/"
 
 
 //SALVAR NOVA TASK
-btnSave.addEventListener('click', function() {
+const btnSave = document.querySelector('#saveTask')
+btnSave.addEventListener('click', () => {
     const newTask = document.querySelector('#newTask').value
     const newDescription = document.querySelector('#description').value
     const prioritySelect = document.querySelector('#priority')
@@ -27,29 +25,51 @@ btnSave.addEventListener('click', function() {
     fetch(urlToDo, submitTask)
 })
 
-
 //LISTA DE TASKS, COM BOTÕES
 let toDoArray = []
 fetch(urlToDo)
     .then(res => res.json())
     .then(toDoList => {
         toDoArray = toDoList
-        toDoHTML()
+        renderToDo()
     })
     .catch(() => {console.log('Erro de requisição!')})   
 
-function toDoHTML(){
+
+//RENDERIZE TASK LIST WITH OPTIONS
+function renderToDo(){
     toDoArray.forEach(task => {
         const taskList = document.querySelector('#taskList')
         const item = document.createElement('li')
-
-        const itemTask = `<h2> ${task.task} </h2>`
+        
+        const itemTask = `<h3> ${task.task} </h3>`
         const itemDescription = `<p> Descrição: ${task.description} </p>`
         const itemPriority = `<p> Prioridade: ${task.priority} </p>`
-        const btnEdit = `<button id="task${task.id}">Editar</button>`
 
-        item.innerHTML = itemTask + itemDescription + itemPriority + btnEdit
-        
+        item.innerHTML = itemTask + itemDescription + itemPriority
         taskList.appendChild(item)
+        
+        const btnDel = document.createElement('button')
+        btnDel.innerHTML = "Remover"
+        btnDel.addEventListener('click', () => {deleteToDo(task.id)})
+        taskList.appendChild(btnDel)
     })
+}
+
+//DELETE TASK
+function deleteToDo(delTask) {
+    const urlDelTask = urlToDo + delTask
+    const removeTask = {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }  
+    }
+    fetch(urlDelTask, removeTask)
+    window.alert('Atividade removida!')
+}
+
+//EDIT TASK
+function editToDo(editTask) {
+
 }
